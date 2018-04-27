83009e2a899e977b24f2bec23958a5b40d0184e6
Abhik Sarkar <sarkarabhik97@gmail.com>
2018-04-27 22:40:05 +0530

    dash code

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,7 +3,53 @@ const router = express.Router();
 const db = require('../config/database.js');
 
 router.get('/',function(req,res){
-    res.send('dashboard page')
+    // console.log(req.user.user_id);
+    var userid = req.user.user_id;
+    res.render('cart', {user:req.user})
 });
+    // res.send('dashboard page'));
+
+
+//active auctions
+router.get('/active', function(req, res){
+    //active ongoing auctions
+    var userid = req.user.user_id;
+    var sqlQ1 = 'select active_auctions_id, end_time, prod_id, prod_name, bid_amt from active_ongoing where user_id=?';
+    db.query(sqlQ1, [userid], function(err, result, fields){
+        // console.log("Active Ongoing");
+        // console.log(result);
+        res.json(result);
+    })
+
+
+
+})
+
+//paid auctions
+router.get('/paid', function(req,res){
+    var userid = req.user.user_id;
+    //won and already paid auctions
+    var sqlQ2 = 'select finished_auctions_id, sold_at_price, timestamp, prod_id, prod_name, status from won where user_id=? and status=1';
+    db.query(sqlQ2, [userid], function(err, result, fields){
+        // console.log("won and already paid auctions");
+        // console.log(result);
+        res.json(result);
+    })
+})
+
+
+//not yet paid auctions
+router.get('/notpaid', function(req, res){
+    var userid = req.user.user_id;
+    //won and yet to pay auctions
+    var sqlQ3 = 'select finished_auctions_id, timestamp, prod_id, prod_name, status from won where user_id=? and status=0';
+    db.query(sqlQ3, [userid], function(err, result, fields){
+        // console.log("won and needs to pay auctions");
+        // console.log(result);
+        res.json(result)
+    })
+})
+
+
 
 module.exports = router;
