c4063e4d76c7fbbdaa366dd4b7b9296c72b2c038
AkshayC <chnakshay@gmail.com>
2018-04-27 16:47:50 +0530

    added script in cicd to restart server if crashed

--- cicd.sh
+++ cicd.sh
@@ -3,6 +3,12 @@
 cd ~/Evaluate
 git remote update
 
+##CICD and server status monitoring
+##1- Restarts the server if git repo has been changed in the next branch
+##2- Restarts the server if the server has crashed
+##3- Does all this in interval of 1,3, or 5 mins.
+##   Currently set to interval of 3 mins
+
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
@@ -25,12 +31,21 @@ if [ "$CHECK" = "lag" ]; then
     echo "check was assigned to lag"
     echo "need pulling"
     git pull
+    killall node
     npm install
-    npm restart
+    npm start
     
 fi
 if [ "$CHECK" = "up-to-date" ]; then
     echo "Check was assigned to up-to-date"
+    if pgrep -x "node" > /dev/null
+    then
+        echo "Server Running Fine"
+    else
+        echo "Server had Stopped"
+        echo "Now restarting the server"
+        npm start
+    fi
 fi
 
 ##echo "ooh shiz der som problem"
